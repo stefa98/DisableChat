3209b34609e43d950db1108cc64de5067e44cd3a
stefa98 <stefapato98@gmail.com>
2017-03-04 22:37:12 +0100

    Update 1.3

--- me/stefa98/DisableChat/Main.java
+++ me/stefa98/DisableChat/Main.java
@@ -15,14 +15,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 
-    public boolean chatEnabled = true;
+    private boolean chatEnabled = true;
 
     @Override
-    public void onEnable(){
+    public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-        this.getConfig().options().copyDefaults(true);
-        this.saveConfig();
-        if(getConfig().getString("Prefix") == null){
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        if (getConfig().getString("Prefix") == null) {
             getConfig().set("Prefix", "&8[&6DisableChat&8]&r");
             getConfig().set("Message", "&cChat Disabilited.");
             getConfig().set("DisabledMessage", "&6The Chat has been disabled!");
@@ -30,68 +30,82 @@ public class Main extends JavaPlugin implements Listener {
             getConfig().set("NoPerm", "&cYou don''t have a permission!");
             getConfig().set("ClearedChat", "&bThe Chat has been cleaned!");
             saveConfig(); //Salvo la config
-        System.out.print("[DisableChat] Plugin by Stefa98 Enabled");
-        this.reloadConfig();
+            System.out.println("[DisableChat] Plugin by Stefa98 Enabled");
+            reloadConfig();
         }
     }
 
     @Override
-    public void onDisable(){
+    public void onDisable() {
         saveConfig();
-        System.out.print("[DisableChat] Plugin by Stefa98 Disabled");
+        System.out.println("[DisableChat] Plugin by Stefa98 Disabled");
     }
 
     @EventHandler
-    public void onChat(AsyncPlayerChatEvent e){
-        if(!chatEnabled && !e.getPlayer().isOp()){
-            for(String s: getConfig().getStringList("Message"))e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', s ));
+    public void onChat(AsyncPlayerChatEvent e) {
+        if (!chatEnabled && !e.getPlayer().isOp()) {
+            for (String s : getConfig().getStringList("Message"))
+                e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', s));
             e.setCancelled(true);
         }
     }
 
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-        if(label.equalsIgnoreCase("dc")) {
-            if (args.length != 0 && args[0] != null) {
-                if (args[0].equalsIgnoreCase("togglechat")) {
-                    if (sender.hasPermission("dc.togglechat")) {
-                        if (chatEnabled) {
-                            chatEnabled = false;
-                            Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Prefix") + getConfig().getString("DisabledMessage")));
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (label.equalsIgnoreCase("dc")) {
+            if (args.length != 0) {
+                switch (args[0]) {
+                    case "togglechat":
+                        if (sender.hasPermission("dc.togglechat")) {
+                            if (chatEnabled) {
+                                chatEnabled = false;
+                                broadcast("DisableMessage");
+                            } else {
+                                chatEnabled = true;
+                                broadcast("EnabledMessage");
+                            }
                         } else {
-                            chatEnabled = true;
-                            Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Prefix") + getConfig().getString("EnabledMessage")));
+                            sender.sendMessage(getFromConfig("NoPerm"));
                         }
-                    } else {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Prefix") + getConfig().getString("NoPerm")));
-                    }
-                }
-                if (args[0].equalsIgnoreCase("clearchat")) {
-                    if (sender.hasPermission("dc.clearchat")) {
-                        for (int n = 0; n < 20; n++) {
-                            Bukkit.broadcastMessage("");
+                        break;
+                    case "clearchat":
+                        if (sender.hasPermission("dc.clearchat")) {
+                            for (int n = 0; n < 20; n++) {
+                                Bukkit.broadcastMessage("");
+                            }
+                            sender.sendMessage(getFromConfig("ClearedChat"));
+                        } else {
+                            sender.sendMessage(getFromConfig("NoPerm"));
                         }
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Prefix") + getConfig().getString("ClearedChat")));
-                    }
+                        break;
+                    case "help":
+                        if (sender.hasPermission("dc.help")) {
+                            sender.sendMessage(ChatColor.YELLOW + "*----------------------*");
+                            sender.sendMessage(ChatColor.GRAY + "DisableCommands Help");
+                            sender.sendMessage(ChatColor.YELLOW + "*----------------------*");
+                            sender.sendMessage(ChatColor.GREEN + "/dc togglechat - Enable/Disable Chat");
+                            sender.sendMessage(ChatColor.GREEN + "/dc clearchat - Clear Chat");
+                        } else {
+                            sender.sendMessage(getFromConfig("NoPerm"));
+                        }
+                        break;
                 }
-                if (args[0].equalsIgnoreCase("help")) {
-                    if (sender.hasPermission("dc.help")) {
-                        sender.sendMessage(ChatColor.YELLOW + "*----------------------*");
-                        sender.sendMessage(ChatColor.GRAY + "DisableCommands Help");
-                        sender.sendMessage(ChatColor.YELLOW + "*----------------------*");
-                        sender.sendMessage(ChatColor.GREEN + "/dc togglechat - Enable/Disable Chat");
-                        sender.sendMessage(ChatColor.GREEN + "/dc clearchat - Clear Chat");
-                    }
-                }
-            }
-            else if (sender.hasPermission("dc.help")) {
+            } else if (sender.hasPermission("dc.help")) {
                 sender.sendMessage(ChatColor.YELLOW + "*----------------------*");
                 sender.sendMessage(ChatColor.GRAY + "Plugin Made by:" + " " + ChatColor.BLUE + "stefa98");
-                sender.sendMessage(ChatColor.GRAY + "Version:" + " " + "1.2");
+                sender.sendMessage(ChatColor.GRAY + "Version:" + " " + "1.3");
                 sender.sendMessage(ChatColor.GRAY + "Help:" + " " + ChatColor.GREEN + "/dc help");
                 sender.sendMessage(ChatColor.YELLOW + "*----------------------*");
             }
         }
-
         return true;
+    }
+
+    private void broadcast(String key) {
+        Bukkit.broadcastMessage(getFromConfig(key));
+    }
+
+    private String getFromConfig(String key) {
+        return ChatColor.translateAlternateColorCodes('&', getConfig().getString("Prefix") + getConfig().getString(key));
     }
 }
